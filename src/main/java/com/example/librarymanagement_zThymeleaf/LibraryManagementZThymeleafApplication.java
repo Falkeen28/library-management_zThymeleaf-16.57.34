e3182b2da626172d8ac08ab67b1d294a93f08e96
Kacper <105564535+Falkeen28@users.noreply.github.com>
2023-12-13 17:00:07 +0100

    Initial commit

---
+++ src/main/java/com/example/librarymanagement_zThymeleaf/LibraryManagementZThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarymanagement_zThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementZThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementZThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/librarymanagement_zThymeleaf/LibraryManagementZThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarymanagement_zThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementZThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
